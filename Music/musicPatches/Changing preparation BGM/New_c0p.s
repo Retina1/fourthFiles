64759255f911886d480e0f8453dfbe0115358d0d
Retina <84415148+Retina1@users.noreply.github.com>
2026-07-11 22:18:37 -0400

    generic palettes, ch8 writing progress

--- Music/musicPatches/Changing preparation BGM/New_c0p.s
+++ Music/musicPatches/Changing preparation BGM/New_c0p.s
@@ -7,18 +7,13 @@
     mov  r0, #0x0
     str  r0, [sp]
 	
-	                    @マップ設定から取得
-@	ldr  r0, =0x0202BCEC	@FE8J
 	ldr  r0, =0x0202BCF0	@FE8U
 	ldrb r0, [r0, #14]
-@	ldr  r1, =#08034520	@FE8J
 	ldr  r1, =0x08034618	@FE8U
 	mov  lr, r1
 	.short 0xF800
 	add  r0, #0x26 @0x72->0x26に変更
 	ldrh r0, [r0]
-@mov	r0, #52
-@	ldr  r1, =0x08002938	@FE8J
 	ldr  r1, =0x080029E8	@FE8U
 	mov  lr, r1
 	mov  r1, r4
--- Music/musicPatches/Changing preparation BGM/c0p.asm
+++ Music/musicPatches/Changing preparation BGM/c0p.asm
@@ -4,18 +4,14 @@
 	mov  r5, r2
 	mov  r6, r3
 	
-	                    ;マップ設定から取得
-;	ldr  r0, =$0202BCEC	;FE8J
 	ldr  r0, =$0202BCF0	;FE8U
 	ldrb r0, [r0, #14]
-;	ldr  r1, =$08034520	;FE8J
 	ldr  r1, =$08034618	;FE8U
 	mov  lr, r1
 	@dcw $F800
 	add  r0, #0x26 ;0x72->0x26に変更
 	ldrh r0, [r0]
-;mov	r0, #52
-;	ldr  r1, =$08002938	;FE8J
+
 	ldr  r1, =$080029E8	;FE8U
 	mov  lr, r1
 	mov  r1, r4
